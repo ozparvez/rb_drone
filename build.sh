1ead954ed806c5e5331cfd4c03643ab4a155007d
ozparvez <88652615+ozparvez@users.noreply.github.com>
2021-08-29 23:21:17 +0600

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Just a basic script U can improvise lateron as per ur need xD 
+
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
+DEVICE=rosemary
+DT_LINK="https://github.com/dualaxis/twrp_redmi_note_10s -b android-11.0"
+DT_PATH=device/xiaomi/$DEVICE
+
+echo " ===+++ Setting up Build Environment +++==="
+apt install openssh-server -y
+apt update --fix-missing
+apt install openssh-server -y
+mkdir ~/twrp && cd ~/twrp
+
+echo " ===+++ Syncing Recovery Sources +++==="
+repo init --depth=1 -u $MANIFEST
+repo sync
+repo sync
+git clone --depth=1 $DT_LINK $DT_PATH
+
+echo " ===+++ Building Recovery +++==="
+. build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+lunch twrp_${DEVICE}-eng && mka bootimage
+
+# Upload zips & recovery.img
+echo " ===+++ Uploading Recovery +++==="
+version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
+OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+
+cd out/target/product/$DEVICE
+mv boot.img ${OUTFILE%.zip}.img
+zip -r9 $OUTFILE ${OUTFILE%.zip}.img
+
+#curl -T $OUTFILE https://oshi.at
+curl -sL $OUTFILE https://git.io/file-transfer | sh
+./transfer wet *.zip
